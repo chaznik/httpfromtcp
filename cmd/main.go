e424a46bd64a69c7d63d7f8d46f34340a8c1ce76
Chazni Katz <chazni.k9@gmail.com>
2026-06-20 00:54:28 +0200

    Read data stream in bytes split on new lines

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"io"
+	"bytes"
+	"errors"
+)
+
+const filePath = "../messages.txt"
 
 func main() {
-    fmt.Println("I hope I get the job!")
+	fd, err := os.Open(filePath)
+
+	if err != nil {
+		log.Fatalf("Error opening file: %s %v\n", filePath, err)
+	}
+	defer fd.Close()
+
+	bytesRead := make([]byte, 8, 8)
+	currentLine := ""
+
+	for {
+		n, err := fd.Read(bytesRead)
+
+		if err != nil {
+			if currentLine != "" {
+				fmt.Printf("read: %s\n", currentLine)
+				currentLine = ""
+			}
+			if errors.Is(err, io.EOF) {
+				break;
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+
+		parts := bytes.Split(bytesRead[:n], []byte("\n"))
+
+		for i:=0; i < len(parts) - 1; i++ {
+			fmt.Printf("read: %s%s\n", currentLine, parts[i])
+			currentLine = ""
+		}
+		currentLine += string(parts[len(parts)-1])
+	}
 }
